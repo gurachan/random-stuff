d1d5d6e301573e5fc1e0905ee849b4d544c39c5f
Dj-jom2x <djjom2x@gmail.com>
2017-04-13 21:43:37 +0800

    node js for normal shell shared cpanel account :)

---
+++ node_installer.sh
@@ -0,0 +1,9 @@
+cd ~
+wget https://nodejs.org/dist/latest/node-v7.9.0-linux-x64.tar.gz
+tar xvf node-v7.9.0-linux-x64.tar.gz
+mv node-v7.9.0-linux-x64 nodejs
+mkdir ~/bin
+cp nodejs/bin/node ~/bin
+ln -s ../nodejs/lib/node_modules/npm/bin/npm-cli.js npm
+node --version
+npm --version
