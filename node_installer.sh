0f5324ca7d27310853aea710805f273a5ace2180
Dj-jom2x <djjom2x@gmail.com>
2017-04-14 17:42:02 +0800

    Update node_installer.sh

--- node_installer.sh
+++ node_installer.sh
@@ -3,8 +3,8 @@ wget https://nodejs.org/dist/latest/node-v7.9.0-linux-x64.tar.gz
 tar xvf node-v7.9.0-linux-x64.tar.gz
 mv node-v7.9.0-linux-x64 nodejs
 mkdir ~/bin
-cd ~/bin
 cp nodejs/bin/node ~/bin
+cd ~/bin
 ln -s ../nodejs/lib/node_modules/npm/bin/npm-cli.js npm
 node --version
 npm --version
